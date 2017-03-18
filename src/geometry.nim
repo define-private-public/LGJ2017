76579ffb46d11c19a88614edd7338f767db945bd
Benjamin N. Summerton <def.pri.pub@gmail.com>
2017-03-18 18:33:13 -0400

    Added some stuff

--- src/geometry.nim
+++ src/geometry.nim
@@ -2,6 +2,10 @@
 
 import basic2d
 
+import strfmt
+
+
+
 type
   Shape2D* = ref Shape2DObj
   Shape2DObj = object of RootObj
@@ -19,6 +23,57 @@ type
     center*: Point2D 
     width*, height*: float  # Should always be postive
     angle*: float           # Rotation, in radians, CCW
+
+
+# the next available ID
+var nextId = 1
+proc getId(): int =
+  result = nextId
+  nextId += 1
+
+
+method `$`*(self: Shape2D): string{.base.} =
+  return "Shape2D [{0}]".fmt(self.id)
+
+
+method `$`*(self: Circle): string =
+  result  = "Circle [{0}]:".fmt(self.id)
+  result &= "  center=" & $self.center
+  result &= "  radius=" & $self.radius
+
+
+method `$`*(self: Rect): string =
+  result  = "Rect [{0}]:".fmt(self.id)
+  result &= "  center=" & $self.center
+  result &= "  width=" & $self.width
+  result &= "  height=" & $self.height
+
+
+# Create a new Circle.
+# c -- center point of the circle
+# r -- radious of the circle, should be non-negative
+proc newCircle*(c: Point2D; r: float): Circle =
+  assert(r >= 0)
+
+  result = new(Circle)
+  result.id = getId()
+  result.center = c
+  result.radius = r
+
+
+# Create a new Circle.
+# c -- center point of the circle
+# w -- width of the rectangle, should be non-negative
+# h -- height of the rectangle, should be non-negative
+proc newRect*(c: Point2D; w, h: float): Rect =
+  assert(w >= 0)
+  assert(h >= 0)
+
+  result = new(Rect)
+  result.id = getId()
+  result.center = c
+  result.width = w
+  result.height = h
 
 
 # TODO proper
