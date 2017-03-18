82d85a24ae960d0d1495f61a2a9d7d29dd74f1c1
Benjamin N. Summerton <def.pri.pub@gmail.com>
2017-03-18 01:49:08 -0400

    Able to bring up a simple OpenGL context!

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ This is my entry for the Linux Game Jam 2017.
 
 It uses:
  - Nim v 0.16.x
- - SDL2 (image, mixer, & ttf)
+ - SDL2 (image, mixer, & ttf), the alternative one
  - OpenGL ES
  - & More! (TODO document "more,")
  - my stopwatch
--- src/game.nim
+++ src/game.nim
@@ -2,6 +2,12 @@
 
 import strfmt
 import stopwatch
+import
+  sdl2/sdl,
+  sdl2/sdl_image as img,
+  sdl2/sdl_ttf as ttf,
+  sdl2/sdl_mixer as mixer
+import opengl
 
 
 type
@@ -15,13 +21,115 @@ type
     deltaTime: float  # Time (in seconds) since last frame
     totalTime: float  # Time (in seconds) since the game has started
 
+  App = ref AppObj
+  AppObj = object
+    window*: sdl.Window     # SDL Window ponter
+    glCtx*: sdl.GLContext   # OpenGL Context
+    running*: bool          # Is the app running?
+
+
+proc newApp(): App =
+  result = App()
+  result.running = false
+
 
 # foward decls
-proc load()     # load & create resources
-proc unload()   # delete resources
-proc init()     # set initial state of game
-proc update(ua: UpdateArguments)    # Update function
-proc draw(da: DrawArguments)        # Render function
+proc initSDL(app: App): bool  # Initlize the SDL2 backend & OpenGL ES 2.0
+proc shutdownSDL(app: App)    # Gracefully shutdown SDL2 and OpenGL
+proc load()                   # load & create resources
+proc unload()                 # delete resources
+proc init()                   # set initial state of game
+proc update(app: App, ua: UpdateArguments)    # Update function
+proc draw(app: App, da: DrawArguments)        # Render function
+
+
+proc initSDL(app: App): bool =
+  result = true
+
+  # init libSDL and it's subsystems
+  if sdl.init(sdl.InitEverything) != 0:
+    echo "Error setting up SDL:"
+    echo sdl.getError()
+    return false
+
+  # SDL_image
+  if img.init(img.InitPng) == 0:
+    echo "Error setting up SDL_image:"
+    echo img.geterror()
+    return false
+
+  # SDL_ttf
+  if ttf.init() != 0:
+    echo "Error setting up SDL_ttf:"
+    echo ttf.geterror()
+    return false
+
+  # SDL_mixer
+  if mixer.init(mixer.InitMod or mixer.InitMp3 or mixer.InitOgg) == 0:
+    echo "Error setting up SDL_mixer:"
+    echo mixer.geterror()
+    return false
+
+  # Setup the mixer
+  if mixer.openAudio(mixer.DefaultFrequency, mixer.Defaultformat, mixer.DefaultChannels, 1024) != 0:
+    echo "Error setting up the mixer:"
+    echo mixer.getError()
+    return false
+
+  # Set opengl version to ES 2.0
+#  discard sdl.glSetAttribute(sdl.GLContextProfileMask, sdl.GLContextProfileEs)
+  discard sdl.glSetAttribute(sdl.GLContextMajorVersion, 3)
+  discard sdl.glSetAttribute(sdl.GLContextMajorVersion, 0)
+
+  # Create the window
+  app.window = sdl.createWindow(
+    "LGJ 2017 (w/ OpenGL ES 2.0)",
+    sdl.WindowPosUndefined,
+    sdl.WindowPosUndefined,
+    960,
+    540,
+    sdl.WindowOpenGL
+  )
+  if app.window == nil:
+    echo "Couldn't setup SDL Window, reason:"
+    echo sdl.getError()
+    return false
+
+  # Setup OpenGL
+  app.glCtx = glCreateContext(app.window)
+  if app.glCtx == nil:
+    echo "Couldn't setup OpenGL ES 2.0 context, reason:"
+    echo sdl.getError()
+    return false
+
+  loadExtensions()
+
+  # Try to use V-Sync
+  if sdl.glSetSwapInterval(1) < 0:
+    echo "Warning: couldn't setup V-Sync, reason:"
+    echo sdl.getError()
+
+  # Set clear color
+  glClearColor(0, 0, 0, 1)
+
+  # Check for error
+  if glGetError() != GL_NoError:
+    echo "There was some issue with OpenGL setup."
+    return false
+
+
+proc shutdownSDL(app: App) =
+  # OpenGL
+  sdl.glDeleteContext(app.glCtx)
+
+  # Window
+  app.window.destroyWindow()
+
+  # SDL & Libraries
+  mixer.quit()
+  ttf.quit()
+  img.quit()
+  sdl.quit()
 
 
 proc load() =
@@ -36,20 +144,34 @@ proc init() =
   echo "Set intial state."
 
 
-proc update(ua: UpdateArguments) =
-  echo "Update #", ua.frameNumber
+proc update(app: App, ua: UpdateArguments) =
+  app.running = ua.totalTime <= 2
 
 
-proc draw(da: DrawArguments) =
-  echo "Draw, dt=", da.deltaTime
-  
+proc draw(app: App, da: DrawArguments) =
+  glClear(GL_ColorBufferBit or GL_DepthBufferBit)
+
+  # TODO replace with shaders!
+  glBegin(GL_Quads)
+  glVertex2f(-0.5, -0.5)
+  glVertex2f( 0.5, -0.5)
+  glVertex2f( 0.5,  0.5)
+  glVertex2f(-0.5,  0.5)
+  glEnd()
+
+  sdl.glSwapWindow(app.window)
 
 
 proc main() =
-
   # Setup
+  var app = newApp()
+  if not initSDL(app):
+    return
+
+  # Get assets
   load()
 
+  # TODO put these in app?
   var
     lastTimeReading = 0.0
     frameCount = 0
@@ -57,10 +179,12 @@ proc main() =
     da = DrawArguments()
     sw = stopwatch()
 
+  # Start
+  app.running = true
   init()
   sw.start()
   var t = sw.secs
-  while t <= 0.1:
+  while app.running:
     # get the delta
     let dt = t - lastTimeReading
 
@@ -74,8 +198,8 @@ proc main() =
     da.totalTime = t
 
     # Update and draw
-    update(ua)
-    draw(da)
+    update(app, ua)
+    draw(app, da)
    
     # Get the next time reading
     lastTimeReading = t
@@ -83,6 +207,8 @@ proc main() =
     frameCount += 1
 
   unload()
+
+  shutdownSDL(app)
 
 
 # Run the game
